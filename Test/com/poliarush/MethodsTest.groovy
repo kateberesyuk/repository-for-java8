13452f280d5a29ee0fdb1b2eb3f7f7f1c35d7e30
Katya <Kate_Beresyuk@ukr.net>
2018-08-21 12:11:11 +0300

    first commit

---
+++ Test/com/poliarush/MethodsTest.groovy
@@ -0,0 +1,7 @@
+package com.poliarush
+
+/**
+ * Created by dsk7 on 8/21/2018.
+ */
+class MethodsTest {
+}
---
+++ src/com/poliarush/Methods.java
@@ -0,0 +1,10 @@
+package com.poliarush;
+
+/**
+ * Created by dsk7 on 8/21/2018.
+ */
+public class Methods {
+    public int CountA1(List<String> list) {
+        
+    }
+}
